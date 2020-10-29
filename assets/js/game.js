2ee9d1214eb5298e14a22ee5479e71330d086458
Ronan <galv.ronan@gmail.com>
2020-10-28 21:45:03 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -118,28 +118,107 @@ var fight = function (enemyName) {
 };
 
 
+// function to start a new game
+var startGame = function() {
+// reset player stats
+playerHealth = 100;
+playerAttack = 10;
+playerMoney = 10;
 
-for (var i = 0; i < enemyNames.length; i++) {
-    //debugger;
-    if (playerHealth > 0) {
-        // let player know what round they are in, remember that arrays start at 0 so it needs to have a 1 added to it
-        window.alert("Welcome to robot Gladiators! Round " + (i + 1));
+    for (var i = 0; i < enemyNames.length; i++) {
+        //debugger;
+        if (playerHealth > 0) {
+            // let player know what round they are in, remember that arrays start at 0 so it needs to have a 1 added to it
+            window.alert("Welcome to robot Gladiators! Round " + (i + 1));
 
-        // picked new enemy to fight based on the index of the enemyNames array
-        var pickedEnemyName = enemyNames[i];
+            // picked new enemy to fight based on the index of the enemyNames array
+            var pickedEnemyName = enemyNames[i];
 
-        // reset enemyHealth before starting a new fight
-        enemyHealth = 50;
+            // reset enemyHealth before starting a new fight
+            enemyHealth = 50;
 
-        // use debugger to pause script from running and check what's going on at that moment in the code
-        // debugger;
+            // use debugger to pause script from running and check what's going on at that moment in the code
+            // debugger;
 
-        //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-        fight(pickedEnemyName);
+            //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            fight(pickedEnemyName);
 
-    // the else below doesn't work!
-    } else {
-        window.alert("You have lost your robot in the battle! Game Over!");
-        break;
+        // the else below doesn't work!
+        } else {
+            window.alert("You have lost your robot in the battle! Game Over!");
+            break;
+        }
     }
-}
+
+    //after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame()
+
+// startGame is cancelled because of endGame()
+//startGame();
+};
+
+
+//function to end the entire game
+var endGame = function() {
+    alert("The game has now ended. Let's see how you did!");
+
+    // if player is still alive, player wins!
+    if (playerHealth > 0){
+        alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+
+    else {
+        alert("You've lost your robot in the battle.");
+    }
+
+    // ask the player if they'd like to play again
+    var playAgainConfirm = confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame()
+    }
+        else {
+            alert("Thank you for playing Robot Gladiators! Come back soon!");
+        }
+
+};
+
+
+// start the game when the page loads
+startGame();
+
+
+
+
+
+
+/*endGame()
+// Alert the player's total stats
+
+// Ask the player if they want to play again
+
+// if yes, call startGame to restart the game
+
+
+
+
+
+
+shop()
+// Ask the player if they want to "shop"
+
+// If no, continue as normal
+
+// If yes, call the shop() function
+
+// ask player if they want to refill health, upgrade attack, or leave
+
+// if refill, subtract money points and increase health of player
+
+// if upgrade, subtract money points form player and increase attack power
+
+// if leave, alert goodbye and exit function
+
+// if any other invalid option, call shop() again
+*/
